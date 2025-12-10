73db3a8bbced31ba49ba6d0469f84269f66838ed
Fizzey <alexis.arduca@epitech.eu>
2025-12-10 16:57:39 +0100

    [UPDATE] - Index.tsx

--- openocean/frontend/src/pages/index.tsx
+++ openocean/frontend/src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { VStack } from "@chakra-ui/react";
 import SectionTitle from "../molecules/SectionTitle";
 import { useNavigate } from "react-router-dom";
 import AvailableImages from "../organisms/AvailableImages";
 import useGetImages from "../hooks/useGetImages";
-import { useEffect, useState } from "react";
 
 const Home: FC = () => {
   const navigate = useNavigate();
   const [images, setImages] = useState<any[]>([]);
-
   const { mutate: getImages } = useGetImages();
 
   useEffect(() => {
@@ -23,14 +21,13 @@ const Home: FC = () => {
     });
   }, [getImages]);
 
-
   return (
     <>
       {images && (
-        < VStack align="start" w="100%" h="100%" spacing="24px">
+        <VStack align="start" w="100%" h="100%" spacing="24px">
           <SectionTitle title="Images" onClick={() => navigate("/images")} />
           <AvailableImages images={images} />
-        </VStack >
+        </VStack>
       )}
     </>
   );
